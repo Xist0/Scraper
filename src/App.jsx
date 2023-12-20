ab0074a726301267f141f6e0ecaab16e06138bef
Xist0 <x1spridcherd@gmail.com>
2023-12-20 21:42:56 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,7 @@ function App() {
             <div className="text-right">
               <span className="pr-3"><h1>Rows Per Page:</h1></span>
               <div className="col-md-2">
-                <div className="d-flex justify-content-end">
-                  <select className="custom-select" name="rowsPerPage" id="changeRows">
-                    <option value="10" selected>10</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="150">150</option>
-                  </select>
-                </div>
+                
               </div>
             </div>
           </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,93 @@
-import React, {useEffect, useState} from 'react'
-import './About.css'
-import { data } from './data.js'
+import React, { useEffect, useState } from 'react';
+import './About.css';
+import { data } from './data.js';
 
 function About() {
+  const itemsPerPageOptions = [10, 20, 50];
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [filteredData, setFilteredData] = useState([]);
 
+  useEffect(() => {
+    // Обновляем filteredData при изменении currentPage или itemsPerPage
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const newData = data.slice(startIndex, endIndex);
+    setFilteredData(newData);
+  }, [currentPage, itemsPerPage]);
 
-  const listItem = data.map(data =>
-    <tr>
-      <td><p>{data.product_store}</p></td>
-      <td><p>{data.product_name}</p></td>
-      <td><p>{data.product_article}</p></td>
-      <td><p>{data.product_in_stock}</p></td>
-      <td id='Prise'><h1>{data.product_price} ₽</h1></td>
-      <td id='link' ><a href={data.product_link}><h1>Перейти</h1></a></td>
-      <td id='Foto'><img className='img' src={data.product_image} alt="" /></td>
-
+  const listItem = filteredData.map((item) => (
+    <tr key={item.product_article}>
+      <td><p>{item.product_store}</p></td>
+      <td><p>{item.product_name}</p></td>
+      <td><p>{item.product_article}</p></td>
+      <td><p>{item.product_in_stock}</p></td>
+      <td id='Prise'><h1>{item.product_price} ₽</h1></td>
+      <td id='link'><a href={item.product_link}><h1>Перейти</h1></a></td>
+      <td id='Foto'><img className='img' src={item.product_image} alt="" /></td>
     </tr>
-  )
+  ));
 
   return (
-
     <div className="about-table">
-      <table>
-        <tr id='table-nav'>
-          <th id='Shop'><h1>Магазин</h1></th>
-          <th id='Shop'><h1>Наименование</h1></th>
-          <th id='Artikyl' ><h1>Артикул</h1></th>
-          <th><h1>В наличии</h1></th>
-          <th id='Prise'><h1>Цена</h1></th>
-          <th id='link'><h1>Ссылка</h1></th>
-          <th id='Foto'><h1>Фото</h1></th>
-        </tr>
-       {listItem}
-      </table>
-    </div>
+      <div className='pagin'>
+        <label htmlFor="itemsPerPage">Элементов на странице: </label>
+        <select
+          id="itemsPerPage"
+          value={itemsPerPage}
+          onChange={(e) => {
+            setItemsPerPage(parseInt(e.target.value, 10));
+            setCurrentPage(1); 
+          }}
+        >
+          {itemsPerPageOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
 
-  )
+      <table>
+        <thead>
+          <tr id='table-nav'>
+            <th><h1>Магазин</h1></th>
+            <th><h1>Наименование</h1></th>
+            <th><h1>Артикул</h1></th>
+            <th><h1>В наличии</h1></th>
+            <th id='Prise'><h1>Цена</h1></th>
+            <th id='link'><h1>Ссылка</h1></th>
+            <th id='Foto'><h1>Фото</h1></th>
+          </tr>
+        </thead>
+        <tbody>{listItem}</tbody>
+      </table>
+
+      <div>
+        {data.length > itemsPerPage && (
+          <div>
+            <button
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              disabled={currentPage === 1}
+            >
+              Предыдущая
+            </button>
+            Страница {currentPage} из {Math.ceil(data.length / itemsPerPage)}
+            <button
+              onClick={() =>
+                setCurrentPage((prev) =>
+                  Math.min(prev + 1, Math.ceil(data.length / itemsPerPage))
+                )
+              }
+              disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+            >
+              Следующая
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-export default About
+
+export default About;
