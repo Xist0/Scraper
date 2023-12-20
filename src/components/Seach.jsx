67906c8d2730ed0b5cb6d3826a5294f4562da9f2
Xist0 <x1spridcherd@gmail.com>
2023-12-20 18:48:31 +0500

    1

--- src/components/Seach.jsx
+++ src/components/Seach.jsx
@@ -2,27 +2,31 @@ import React from 'react'
 import {useState} from 'react';
 
 const Seach = () => {
-    const [message, setMessage] = useState('');
-
-    const handleChange = event => {
-      setMessage(event.target.value);
+    const [text, setText] = useState('');
+    const [displayText, setDisplayText] = useState('');
   
-      console.log('value is:', event.target.value);
+    const handleSubmit = e => {
+      e.preventDefault();
+      setDisplayText(text)
+      
+    };
+  
+    const handleChange = e => {
+      setText(e.target.value);
+      
     };
   
     return (
-      <div>
-        <input
-          type="text"
-          id="message"
-          name="message"
-          onChange={handleChange}
-          value={message}
-        />
-  
-        <h2>Message: {message}</h2>
-        <button>213123          </button>
-      </div>
+      <>
+        <form onSubmit={e => handleSubmit(e)}>
+          <label>Text: </label>
+          <input type="text" value={text} onChange={e => handleChange(e)} />
+          <button type="submit" onClick={handleSubmit}>
+            Show
+          </button>
+          {displayText && <p>{displayText}</p>}
+        </form>
+      </>
     );
 }
 
