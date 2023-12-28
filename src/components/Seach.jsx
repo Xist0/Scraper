47c72f224910f43ff8db976c46910b3cbe37198f
Xist0 <x1spridcherd@gmail.com>
2023-12-28 10:33:56 +0500

    2

--- src/components/Seach.jsx
+++ src/components/Seach.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import {useState} from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-const Seach = () => {
-    const [text, setText] = useState('');
-    const [displayText, setDisplayText] = useState('');
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-      setDisplayText(text)
-      
-    };
-  
-    const handleChange = e => {
-      setText(e.target.value);
-      
-    };
-  
-    return (
-      <>
-        <form onSubmit={e => handleSubmit(e)}>
-          <label>Text: </label>
-          <input type="text" value={text} onChange={e => handleChange(e)} />
-          <button type="submit" onClick={handleSubmit}>
-            Show
-          </button>
-          {displayText && <p>{displayText}</p>}
-        </form>
-      </>
-    );
-}
+const Search = () => {
+  const { register, handleSubmit } = useForm();
+  const [inputValue, setInputValue] = useState('');
 
-export default Seach
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (data) => {
+    try {
+      // Преобразование введенного текста в JSON
+      const jsonData = JSON.stringify({ data: data.name });
+      console.log('JSON данных:', jsonData);
+    } catch (error) {
+      console.error('Ошибка при преобразовании в JSON:', error);
+    }
+  };
+
+  return (
+    <div className="card-body w-full lg:w-96">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Text: </label>
+        <input
+          {...register('name', { required: true })}
+          type="text"
+          placeholder="name"
+          className="input input-bordered"
+          value={inputValue}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Найти</button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
